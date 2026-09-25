291af7afc73c130ecb88f1c7efeba6893aadc899
joi0046 <220693654+joi0046@users.noreply.github.com>
2026-09-25 14:38:58 +0900

    センサーの設定を変更

--- app/gateway/drivers/hdc1000/sensor.go
+++ app/gateway/drivers/hdc1000/sensor.go
@@ -2,19 +2,28 @@ package hdc1000
 
 import (
 	"fmt"
+
 	"gateway/internal/driver"
+	"gateway/internal/i2c"
 )
 
 const (
+	ManufacturerRegister = 0xFE
+	DeviceRegister       = 0xFF
+
 	ManufacturerID uint16 = 0x5449
 	DeviceID       uint16 = 0x1000
-
-	ManufacturerRegister uint8 = 0xFE
-	DeviceRegister       uint8 = 0xFF
 )
 
 type Sensor struct {
-	Address uint8
+	bus     *i2c.Bus
+	address uint8
+}
+
+func New(bus *i2c.Bus) *Sensor {
+	return &Sensor{
+		bus: bus,
+	}
 }
 
 func (s *Sensor) Name() string {
@@ -22,10 +31,29 @@ func (s *Sensor) Name() string {
 }
 
 func (s *Sensor) Detect(info driver.DeviceInfo) bool {
-	return info.Address == 0x40 ||
-		info.Address == 0x41 ||
-		info.Address == 0x42 ||
-		info.Address == 0x43
+	if info.Address < 0x40 || info.Address > 0x43 {
+		return false
+	}
+
+	if err := s.bus.SetAddress(info.Address); err != nil {
+		return false
+	}
+
+	manufacturer, err := s.bus.ReadRegister(ManufacturerRegister, 2)
+	if err != nil {
+		return false
+	}
+
+	device, err := s.bus.ReadRegister(DeviceRegister, 2)
+	if err != nil {
+		return false
+	}
+
+	manufacturerID := uint16(manufacturer[0])<<8 | uint16(manufacturer[1])
+	deviceID := uint16(device[0])<<8 | uint16(device[1])
+
+	return manufacturerID == ManufacturerID &&
+		deviceID == DeviceID
 }
 
 func (s *Sensor) Read() (driver.Reading, error) {
