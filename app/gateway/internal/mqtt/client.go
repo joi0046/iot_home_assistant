fe10c67b6b700c535aaa7e6c56eff73fe4f094e3
joi0046 <220693654+joi0046@users.noreply.github.com>
2026-09-25 22:09:33 +0900

    ドキュメントの追加

---
+++ app/gateway/internal/mqtt/client.go
@@ -0,0 +1,44 @@
+package mqtt
+
+import (
+	"fmt"
+
+	paho "github.com/eclipse/paho.mqtt.golang"
+)
+
+type Client struct {
+	client paho.Client
+}
+
+func New(brokerURL string) (*Client, error) {
+	opts := paho.NewClientOptions()
+	opts.AddBroker(brokerURL)
+
+	client := paho.NewClient(opts)
+
+	token := client.Connect()
+	token.Wait()
+
+	if err := token.Error(); err != nil {
+		return nil, fmt.Errorf("MQTT connect: %w", err)
+	}
+
+	return &Client{
+		client: client,
+	}, nil
+}
+
+func (c *Client) Publish(topic string, payload string) error {
+	token := c.client.Publish(topic, 0, false, payload)
+	token.Wait()
+
+	if err := token.Error(); err != nil {
+		return fmt.Errorf("MQTT publish: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Client) Close() {
+	c.client.Disconnect(250)
+}
