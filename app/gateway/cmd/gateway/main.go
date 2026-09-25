5fdb5de2f18e124a331607a5a46289fb98acb8c3
joi0046 <220693654+joi0046@users.noreply.github.com>
2026-09-25 14:35:17 +0900

    アドレスに干渉させる

--- app/gateway/cmd/gateway/main.go
+++ app/gateway/cmd/gateway/main.go
@@ -34,6 +34,13 @@ func main() {
 		log.Fatal(err)
 	}
 
+	data, err = bus.ReadRegister(0xFF, 2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Device ID: 0x%02X%02X\n", data[0], data[1])
+
 	fmt.Printf("Manufacturer ID: 0x%02X%02X\n", data[0], data[1])
 	// Driver Registry
 	registry := driver.NewRegistry(
