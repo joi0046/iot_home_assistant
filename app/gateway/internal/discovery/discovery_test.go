8c12ec5aee74c657bf4d8a6348bf26c3a0f3f5d0
joi0046 <220693654+joi0046@users.noreply.github.com>
2026-09-26 21:59:31 +0900

    discoverytestをアドレスを見れるようにした

--- app/gateway/internal/discovery/discovery_test.go
+++ app/gateway/internal/discovery/discovery_test.go
@@ -78,3 +78,48 @@ func TestDiscoveryScanError(t *testing.T) {
 		)
 	}
 }
+
+func TestParseOutput(t *testing.T) {
+	output := `
+     0 1 2 3 4 5 6 7 8 9 a b c d e f
+00:                         -- -- -- -- -- -- -- --
+10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+40: 40 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+70: -- -- -- -- -- -- 76 -- -- -- -- -- -- -- -- --
+`
+
+	devices := parseOutput(output, "1")
+
+	if len(devices) != 2 {
+		t.Fatalf(
+			"len(devices) = %d, want 2",
+			len(devices),
+		)
+	}
+
+	if devices[0].Bus != "i2c-1" {
+		t.Errorf(
+			"devices[0].Bus = %q, want %q",
+			devices[0].Bus,
+			"i2c-1",
+		)
+	}
+
+	if devices[0].Address != 0x40 {
+		t.Errorf(
+			"devices[0].Address = 0x%02X, want 0x40",
+			devices[0].Address,
+		)
+	}
+
+	if devices[1].Address != 0x76 {
+		t.Errorf(
+			"devices[1].Address = 0x%02X, want 0x76",
+			devices[1].Address,
+		)
+	}
+}
